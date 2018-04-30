97d9852f464d6ea496395b10d6524a3be924711f
Cernea Victor <cernia.viktor@rambler.ru>
2018-04-30 14:17:54 +0000

    Everything

---
+++ js/forvue.js
@@ -0,0 +1,32 @@
+app = new Vue ({
+	el: '#banner',
+	
+	data: {
+		title: "Negru de Purcari",
+		wineimg: "https://cdn.shopify.com/s/files/1/1201/9192/products/vinohora_rosu_2014_1024x1024.png?v=1457518879",
+		tip: "detaliul",
+		details: "Negru de Purcari is the king of red wines – noble, with a rich structure and a perfect bouquet."
+	}
+});
+
+// Make a request for a user with a given ID
+// axios.get('/user?ID=12345')
+// 	.then(function (response) {
+// 		console.log(response);
+// 	})
+// 	.catch(function (error) {
+// 		console.log(error);
+// 	});
+	
+// 	// Optionally the request above could also be done as
+// 	axios.get('/user', {
+// 		params: {
+// 			ID: 12345
+// 		}
+// 	})
+// 	.then(function (response) {
+// 		console.log(response);
+// 	})
+// 	.catch(function (error) {
+// 		console.log(error);
+// });
